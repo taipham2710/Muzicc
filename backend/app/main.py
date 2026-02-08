b3b57f8168fe7c1b40cc4cc20088d9165d47a99f
taipham2710 <taipham.dev@gmail.com>
2026-02-09 05:14:44 +0700

    fix(cors): enable CORS for frontend localhost

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from app.api import auth, songs, health
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="MUZICC Backend API")
 
 app.include_router(health.router, prefix="/health", tags=["health"])
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(songs.router, prefix="/songs", tags=["songs"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
